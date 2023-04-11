879e86f56d883d5afb2751e844699df749457e67
wedis <wedis@10.0.0.91>
2023-04-11 18:33:14 -0400

    Initial Commit

---
+++ src/day3hwsaba/SabaClass.java
@@ -0,0 +1,13 @@
+package day3hwsaba;
+
+public class SabaClass {
+
+	public void add() {
+		System.out.println("a+b");
+
+	}
+
+	public void add1(int a, int b) {
+		System.out.println("a+b");
+	}
+}
