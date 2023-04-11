1e260410041ae15e210eca467766b15ffb98b5cc
GYTumlisan <gymoa2011@gmail.com>
2023-04-10 21:44:06 -0700

    Initial commit with two methods

---
+++ src/Day3HWGary/Day3HW_Test.java
@@ -0,0 +1,18 @@
+package Day3HWGary;
+
+public class Day3HW_Test {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Test......");
+	}
+	
+	public void add(int a, int b) {
+		System.out.println(a + b);
+	}
+	
+	public void multiply(int a, int b) {
+		System.out.println(a * b);
+	}
+
+}
