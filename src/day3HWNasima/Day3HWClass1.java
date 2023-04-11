836e8ca05881f6737cc4d3951c5b3939099a0f9d
nasim <nasim@10.0.0.10>
2023-04-11 01:09:35 -0400

    Rename the package

---
+++ src/day3HWNasima/Day3HWClass1.java
@@ -0,0 +1,20 @@
+package day3HWNasima;
+
+public class Day3HWClass1 {
+	
+	public static void main(String[] args) {
+		
+		
+	}
+	
+	
+	public int Sum(int a, int b) {
+		
+		return a+b;
+	}
+	
+	protected void getInfo() {
+		System.out.println("This is first class");
+	}
+
+}
---
+++ src/day3HWNasima/Day3HWClass2.java
@@ -0,0 +1,25 @@
+package day3HWNasima;
+
+public class Day3HWClass2 {
+
+	public static void main(String[] args) {
+		
+		Day3HWClass2 obj = new Day3HWClass2();
+		getAddress();
+		
+		int r=obj.divide(4, 2);
+		System.out.println(r);
+		
+		
+
+	}
+
+	public static void getAddress() {
+		System.out.println("123 Street");
+
+	}
+
+	public int divide(int a, int b) {
+		return a / b;
+	}
+}
