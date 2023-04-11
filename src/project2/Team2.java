a02fa375242910f6170ce03899b93c6c9bb03241
farahabdullahi <farahabdullahi@192.168.4.25>
2023-04-11 19:30:42 -0400

    new comment added by farah

---
+++ src/project2/Team2.java
@@ -0,0 +1,21 @@
+package project2;
+
+public class Team2 {
+	
+	public void exam1() {
+		System.out.println("exam is comeing soon...");
+	}
+	
+	public void finalTerm() {
+		
+		System.out.println("Last month");
+		
+		
+		
+		String name = "Ali";
+	}
+	
+	
+	// this is new comment from farah
+
+}
