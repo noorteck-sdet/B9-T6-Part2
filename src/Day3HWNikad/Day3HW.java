43590830dba717689493f8c96c66aff4e6f89705
mohammadpanahnikzad <mohammadpanahnikzad@localhost>
2023-04-11 16:54:13 -0400

    New Commit...

---
+++ src/Day3HWNikad/Day3HW.java
@@ -0,0 +1,12 @@
+package Day3HWNikad;
+
+public class Day3HW {
+	
+	public static void main(String args []) {
+		
+		System.out.println("Day 3 HomeWrok...");
+	}
+     public void addNew() {
+    	 System.out.println("New method.....");
+     }
+}
