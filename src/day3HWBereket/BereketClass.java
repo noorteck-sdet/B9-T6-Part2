b9a3464da83db11a19bcd66a0d2124ff29c9b963
bruka <bruka@LAPTOP-43QTSTF8>
2023-04-11 14:43:47 -0700

    Initial Commit

---
+++ src/day3HWBereket/BereketClass.java
@@ -0,0 +1,17 @@
+package day3HWBereket;
+
+public class BereketClass {
+
+	public static void main(String[] args) {
+
+		System.out.println("This is Push check...");
+	}
+
+	public void sdetClass() {
+		System.out.println("Fahim NTK");
+	}
+
+	public void tutorClass() {
+		System.out.println("Muhammet Ozen");
+	}
+}
