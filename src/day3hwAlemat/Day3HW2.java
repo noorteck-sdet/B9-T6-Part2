b8988dcc0cb0edb7776fb7c63929d5816f1ae60a
Alemat <alema@10.0.0.114>
2023-04-12 19:25:17 -0400

    added more comment

---
+++ src/day3hwAlemat/Day3HW2.java
@@ -0,0 +1,26 @@
+package day3hwAlemat;
+
+public class Day3HW2 {
+	
+		private String name;
+		private int age;
+		private char gender;
+	
+	public String getName() {
+		return name;
+	}
+ public int getAge() {
+	return age;
+	 
+ }
+public void setAge(int age)	{
+	this.age = age;
+}
+public char getGender() {
+	return gender;
+	
+}
+// This is practice time
+}
+
+
