27cb8ad7d2bc6ac17b2a67ca41499f02af7b0055
wedis <wedis@10.0.0.91>
2023-04-12 19:25:19 -0400

    added comment

---
+++ src/day3hwsaba/Day3HWClass1.java
@@ -0,0 +1,22 @@
+package day3hwsaba;
+
+public class Day3HWClass1 {
+	
+	public static void main(String[] args) {
+		
+		
+		}
+	public void add1(int a, int b) {
+		System.out.println("a+b");
+	}
+//Saba is practicing
+
+//Practice more
+	//practice makes perfect
+
+public void add2(int a, int b) {
+	System.out.println("a+b");
+	
+}
+//Trying again
+}
